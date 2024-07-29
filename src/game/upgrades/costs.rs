a0e14e7cde142c417b069944458cdb3f59889fbc
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2024-07-30 04:18:13 +1200

    lowered costs

--- src/game/upgrades/costs.rs
+++ src/game/upgrades/costs.rs
@@ -2,23 +2,25 @@ use std::time::Duration;
 
 pub const STARTING_CYCLES: u32 = compute_ring_cost(0) + compute_electron_cost(0, 0);
 
+pub const COST_SCALE: u32 = 2;
+
 pub const fn compute_electron_cost(rings: usize, electrons: usize) -> u32 {
     match (rings, electrons) {
-        (0, 0) => 0,
+        (_, 0) => 0,
         (0, 1) => 5,
-        (_, _) => 5 * (electrons as u32 + 1) * (rings as u32 + 1).pow(2),
+        (_, _) => 2 * (electrons as u32 + 1) * (rings as u32 + 1).pow(2) / COST_SCALE,
     }
 }
 
 pub const fn compute_ring_cost(rings: usize) -> u32 {
     match rings {
         0 => 0,
-        _ => 25 * (rings as u32).pow(2),
+        _ => 25 * (rings as u32).pow(2) / COST_SCALE,
     }
 }
 
 pub fn compute_speed_cost(rings: usize, level: u32) -> u32 {
-    (4 * (level + 1)) * (rings as u32 + 1).pow(2)
+    (4 * (level + 1)) * (rings as u32 + 1).pow(2) / COST_SCALE
 }
 
 pub fn compute_cycle_cost(rings: usize, duration: Option<Duration>) -> u32 {
@@ -27,5 +29,5 @@ pub fn compute_cycle_cost(rings: usize, duration: Option<Duration>) -> u32 {
         (3. / secs) as u32 + 5
     } else {
         3
-    }) * (rings as u32 + 1).pow(2)
+    }) * (rings as u32 + 1).pow(2) / COST_SCALE
 }
