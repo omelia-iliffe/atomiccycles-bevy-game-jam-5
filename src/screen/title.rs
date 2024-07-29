d44f45e7663b087e064239882c90b9c22c468337
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2024-07-29 21:59:12 +1200

    added title images

--- src/screen/title.rs
+++ src/screen/title.rs
@@ -1,7 +1,7 @@
 //! The title screen that appears when the game starts.
 
 use bevy::prelude::*;
-
+use bevy::render::texture::{ImageLoaderSettings, ImageSampler};
 use super::Screen;
 use crate::ui::prelude::*;
 
@@ -22,7 +22,7 @@ enum TitleAction {
     Exit,
 }
 
-fn enter_title(mut commands: Commands) {
+fn enter_title(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .ui_root()
         .insert(StateScoped(Screen::Title))
@@ -33,6 +33,29 @@ fn enter_title(mut commands: Commands) {
             #[cfg(not(target_family = "wasm"))]
             children.button("Exit").insert(TitleAction::Exit);
         });
+    commands.spawn(
+        (
+            Name::new("title image"),
+            StateScoped(Screen::Title),
+            ImageBundle {
+                style: Style {
+                    margin: UiRect::all(Val::Auto),
+                    ..default()
+                },
+                z_index: ZIndex::Global(-100),
+                image: UiImage::new(asset_server.load_with_settings(
+                    // This should be an embedded asset for instant loading, but that is
+                    // currently [broken on Windows Wasm builds](https://github.com/bevyengine/bevy/issues/14246).
+                    "images/title_image_no_middle.png",
+                    |settings: &mut ImageLoaderSettings| {
+                        // Make an exception for the splash image in case
+                        // `ImagePlugin::default_nearest()` is used for pixel art.
+                        settings.sampler = ImageSampler::linear();
+                    },
+                )),
+                ..default()
+            },
+        ));
 }
 
 fn handle_title_action(
