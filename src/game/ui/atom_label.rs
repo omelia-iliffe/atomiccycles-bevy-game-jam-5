22a03df6b9d1085b8b4e6bc3e8b0f6c2c3d4cbcd
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2024-07-30 03:48:54 +1200

    fixed atom name default text

--- src/game/ui/atom_label.rs
+++ src/game/ui/atom_label.rs
@@ -15,15 +15,11 @@ pub(super) fn plugin(app: &mut App) {
 pub struct SpawnAtomLabel;
 
 fn spawn_cycle_ui(_trigger: Trigger<SpawnAtomLabel>, mut commands: Commands) {
-    // The only thing we have in our level is a player,
-    // but add things like walls etc. here.
-    // commands.trigger(SpawnPlayer);
 
     commands.spawn((
         TextBundle::from_sections([
             TextSection::new(
-                // Accepts a `String` or any type that converts into a `String`, such as `&str`
-                "Cycles: ",
+                "",
                 TextStyle {
                     font_size: 40.0,
                     color: BUTTON_TEXT,
